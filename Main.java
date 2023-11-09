26f6d175ef4beaba1d81f8a0441badd554d61678
Mrgumball2004 <arjie1419@gmail.com>
2023-11-09 19:00:47 +0800

    added main

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+import main.Action;
+import main.Cat;
+
+public class Main {
+
+    public static void main(String[] args) {
+      
+        Action action = new Action("Gumball");
+        
+        action.purr();
+        action.blue();                     
+    
+        Cat meow = new Cat("Gumball");
+        
+        meow.whiskers(); 
+        
+   }      
+}
